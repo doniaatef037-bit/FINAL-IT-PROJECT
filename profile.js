959cf007b9d8340c2ae44cb0fd6464608a962ca6
doniaatef037-bit <doniaatef037@gmail.com>
2026-04-30 18:16:27 +0300

    Refactor window.onload and logout functions

--- profile.js
+++ profile.js
@@ -4,9 +4,8 @@ window.onload = function() {
 
     if (data) {
         let user = JSON.parse(data);
-        let realPassword = user.pass; // الباسورد الحقيقي
+        let realPassword = user.pass; 
 
-        // عرض البيانات الأساسية
         document.getElementById("pName").innerText = user.fName;
         document.getElementById("pEmail").innerText = user.email;
         document.getElementById("pAvatar").innerText = user.fName[0].toUpperCase();
@@ -14,17 +13,16 @@ window.onload = function() {
         let passSpan = document.getElementById("pPassword");
         let toggleIcon = document.getElementById("toggleIcon");
 
-        // --- السطر السحري: بيخلي عدد النجوم = عدد حروف الباسورد ---
+ 
         passSpan.innerText = "•".repeat(realPassword.length); 
 
         toggleIcon.onclick = function() {
             if (toggleIcon.classList.contains("fa-eye-slash")) {
-                // إظهار الباسورد الحقيقي
-                passSpan.innerText = realPassword;
+             passSpan.innerText = realPassword;
                 toggleIcon.classList.replace("fa-eye-slash", "fa-eye");
                 toggleIcon.style.color = "#ffb703";
             } else {
-                // إرجاع النجوم بنفس عدد الحروف
+            
                 passSpan.innerText = "•".repeat(realPassword.length);
                 toggleIcon.classList.replace("fa-eye", "fa-eye-slash");
                 toggleIcon.style.color = "#8a99a4";
@@ -36,12 +34,11 @@ window.onload = function() {
 };
 
 function logout() {
-    // 1. نمسح اليوزر الحالي من الـ Storage عشان الموقع ميعرفوش
+    
     localStorage.removeItem("currentUser");
     
-    // 2. رسالة اختيارية تظهر لثانية (ممكن تستغني عنها)
     console.log("Logging out...");
 
-    // 3. نوديه لصفحة الـ Login
+   
     window.location.href = "login.html"; 
 }
